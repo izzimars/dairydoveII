fe01bf69be51fbcebe6dbfae29cc7043fcfa3bdf
adanni3 <akanjii040@gmail.com>
2024-07-17 15:33:58 +0100

    email and config

--- services/emailServices.js
+++ services/emailServices.js
@@ -35,11 +35,25 @@ const sendEmail = async (
   }
 };
 
+
+
 const sendOtpEmail = async (user_email, otp) => {
   try {
-    subject = "Verify Your Email";
-    html = `<p>Enter <b>${otp}</b> in the app to complete your verification.</p>. OTP expires in 6 minutes</p>`;
-    await sendEmail(user_email, subject, html);
+    const subject = "Verify Your Email";
+    const logoURL = `https://res.cloudinary.com/dwykmvdhb/image/upload/v1721222788/xn1fblohfrvwopzcmaq3.png`
+    const html = `
+      <div style="background-color: #f0f0f0; padding: 20px;">
+        <section style="max-width: 600px; margin: auto; background-color: white; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+          <div style="display:block;"><img src= "${logoURL}" alt="Diary Dove logo" style="width:43px; height:36px; display:inline;">
+            <h1 style="color: #DA9658; display:inline; ">Dairy Dove</h2>
+          </div>
+          <h3>Email Verification</h3>
+          <p>Enter <b>${otp}</b> in the app to complete your verification. OTP expires in 6 minutes.</p>
+          <p>Ignore this message if you have already been verified.</p>
+        </section>
+      </div>`;
+    await sendEmail(user_email, subject, "", html);
+    console.log(otp)
   } catch (err) {
     logger.error(err.message);
     throw err;
@@ -47,3 +61,4 @@ const sendOtpEmail = async (user_email, otp) => {
 };
 
 module.exports = { sendOtpEmail, sendEmail };
+
